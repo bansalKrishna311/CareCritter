63b8b97823e908cb927db1b1b21e9c1e68b13852
bansalKrishna311 <bansalkrishna311@gmail.com>
2023-10-26 14:43:12 +0530

    mongo

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const mongoUrl = require("./keys");
 
-mongoose.connect="mongodb+srv://CareCritter:<password>@carecritter.9unm2ks.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongoUrl);
+
+mongoose.connection.on("connect", () => {
+  console.log("Successfully Connected to mongo");
+});
+
+mongoose.connection.on("error ", () => {
+    console.log("not connected to mongodb");
+  });
+  
+
+
+
 
 app.listen(port, () => console.log("Server is running on port" + " " + port));
---
+++ Backend/keys.js
@@ -0,0 +1,4 @@
+mongoose.connect="mongodb+srv://CareCritter:VauXlreAeq913bds@carecritter.9unm2ks.mongodb.net/?retryWrites=true&w=majority"
+
+
+module.exports = mongoUrl;
