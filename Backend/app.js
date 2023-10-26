1fecde1f57c3e05805b41c7ec92a1084ffe44066
bansalKrishna311 <bansalkrishna311@gmail.com>
2023-10-26 14:15:38 +0530

    save

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
 
+mongoose.connect
 
-app.listen(port, () => console.log("Server is running on port"+ " " +  port));
+app.listen(port, () => console.log("Server is running on port" + " " + port));
